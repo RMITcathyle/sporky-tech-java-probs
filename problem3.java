221dd8b740f735f82bc0c2c543e6c7242cbdf13f
Vidhathra <s3944867@student.rmit.edu.au>
2022-05-17 14:35:52 +1000

    commit

--- problem3.java
+++ problem3.java
@@ -1,3 +1,24 @@
 public class problem3 {
-    
+    public static void main(String[]args) {
+        
+        boolean hot;
+        boolean cold;
+        int temperature = 25;
+
+        hot = temperature > 25;
+        cold = temperature < 20;
+
+        System.out.print("Sporky needs ");
+
+        if (hot) {
+            System.out.print("a fan.");
+        }
+        else if (cold) {
+            System.out.print("a scarf.");
+        }
+        else {
+            System.out.print("nothing.");
+        }
+
+}
 }
