0d64daf223ab508cc2e69126ada1e5ce2766489d
Vidhathra <s3944867@student.rmit.edu.au>
2022-05-20 13:01:34 +1000

    commit

--- problem4.java
+++ problem4.java
@@ -1,36 +1,14 @@
-//fourth option
 import java.util.Scanner;
 
 public class problem4 {
     public static void main(String[] args) {
-        Scanner inSS = null;
-        String[] material = new String[4];
-        String MATERIAL;    //input string
-        String[] colour = new String[3];
-        String COLOUR;      //input string
-        int i;
+        Scanner inputCost = null;
+        int[] cost = new int[2];
+        String travelCost;
 
-        MATERIAL = "COTTON CASHMERE ALPACA SATIN";
-
-        inSS = new Scanner(MATERIAL);
-
-        for(i=0; i<4; ++i) {
-            material[i] = inSS.next();
-            System.out.println(material[i]);
-        }
-
-        COLOUR = "RED HOTPINK BURGUNDY";
-
-        inSS = new Scanner(COLOUR);
-
-        for(i=0; i<3; ++i) {
-            colour[i] = inSS.next();
-            System.out.println(colour[i]);
-        }
+        travelCost = ""
 
         
-        
-       inSS.close();
     }
     
 }
