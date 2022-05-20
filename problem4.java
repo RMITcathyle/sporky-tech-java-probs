507466bcaff775ec83914266fe22cd7579a93655
Vidhathra <s3944867@student.rmit.edu.au>
2022-05-20 11:29:28 +1000

    commit

--- problem4.java
+++ problem4.java
@@ -1,12 +1,23 @@
 //fourth option
+import java.util.Scanner;
 
 public class problem4 {
     public static void main(String[] args) {
-        String[] material = new String[3];
-        String material
+        Scanner inSS = null;
+        String[] material = new String[4];
+        String MATERIAL;    //input string
+        int i;
 
-        material
+        MATERIAL = "COTTON CASHMERE ALPACA SATIN";
 
+        inSS = new Scanner(MATERIAL);
+
+        for(i=0; i<4; ++i) {
+            material[i] = inSS.next();
+            System.out.println(material[i]);
+        }
+        
+       inSS.close();
     }
     
 }
