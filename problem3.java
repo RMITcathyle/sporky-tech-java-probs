15ce9f9110e6ad4f35caa8acdd63cbe1e22924da
Vidhathra <s3944867@student.rmit.edu.au>
2022-05-20 11:29:38 +1000

    commit

--- problem3.java
+++ problem3.java
@@ -5,23 +5,23 @@ import java.util.Scanner;
 public class problem3 {
         public static void main(String[]args) {
             Scanner scnr = new Scanner(System.in);
-            boolean hot;
-            boolean cold;
-            int temperature = scnr.nextInt();
-            
-            hot = temperature > 25;
-            cold = temperature < 20;
-            
-            System.out.print("Sporky needs ");
+                boolean hot;
+                boolean cold;
+                int temperature = scnr.nextInt();
+                
+                hot = temperature > 25;
+                cold = temperature < 20;
+                
+                System.out.print("Sporky needs ");
 
-            if (hot) {
-                System.out.print("a fan.");
-            }
-            else if (cold){
-                System.out.print("a scarf.");
-            }
-            else {
-                System.out.print("nothing.");
+                if (hot) {
+                    System.out.print("a fan.");
+                }
+                else if (cold){
+                    System.out.print("a scarf.");
+                }
+                else {
+                    System.out.print("nothing.");
+                }
             }
         }
-    }
